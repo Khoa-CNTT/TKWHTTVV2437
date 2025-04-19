6a9a03d742f189c33610c806205a67678a7df634
NgocPhucDn03 <ngocphucdn06@gmail.com>
2025-04-19 17:11:04 +0700

    .

---
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const dashboard = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">Admin Dashboard</h1>
+        <p className="mt-4 text-lg">Welcome to the admin dashboard!</p>
+        </div>
+    );
+}
+export default dashboard;
